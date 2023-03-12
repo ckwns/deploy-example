6c763a2b7943a7b5acd3984c8ba1c84658fb822d
ckwns <ck_wns@yahoo.com>
2023-03-12 17:39:14 -0400

    Update test_fixtures.py

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,5 +1,6 @@
 import pytest
 
+# basic test case to add required build checks and statuses
 @pytest.fixture
 def example_fixture():
     return 1
@@ -7,6 +8,6 @@ def example_fixture():
 def test_with_fixture(example_fixture):
     assert example_fixture == 1
 
-    
+# project related test cases    
 def test_with_fixture1(example_fixture):
-    assert example_fixture == 2
+    assert example_fixture == 1
